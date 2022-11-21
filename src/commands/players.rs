a3993f65ddbfd59a380b746bf8d6a11b5842a5ab
laundmo <laurinschmidt2001@gmail.com>
2022-11-21 16:49:30 +0100

    handle empty api response

--- src/commands/players.rs
+++ src/commands/players.rs
@@ -24,36 +24,46 @@ pub async fn online(ctx: Context<'_>) -> Result<(), Error> {
     let response =
         reqwest::get("https://gvrfunctions.azurewebsites.net/api/listonlineplayers").await?;
     let users: Vec<Users> = response.json().await?;
-    ctx.send(|m| {
-        m.embed(|b| {
-            b.title("Grapple Tournament stats")
-                .description(format!(
-                    "Total Players: {}",
-                    users.iter().map(|u| u.online_player_count).sum::<u32>()
-                ))
-                .fields(vec![
-                    (
-                        "Region",
-                        users
-                            .iter()
-                            .map(|u| u.region.as_ref().to_string())
-                            .collect::<Vec<String>>()
-                            .join("\n"),
-                        true,
-                    ),
-                    (
-                        "Online",
-                        users
-                            .iter()
-                            .map(|u| format!("{}", u.online_player_count))
-                            .collect::<Vec<String>>()
-                            .join("\n"),
-                        true,
-                    ),
-                ])
-                .footer(|f| f.text("Excluding players in private matches"))
+    if users.is_empty() {
+        ctx.send(|m| {
+            m.embed(|b| {
+                b.title("Grapple Tournament stats")
+                    .description("No players online")
+            })
         })
-    })
-    .await?;
+        .await?;
+    } else {
+        ctx.send(|m| {
+            m.embed(|b| {
+                b.title("Grapple Tournament stats")
+                    .description(format!(
+                        "Total Players: {}",
+                        users.iter().map(|u| u.online_player_count).sum::<u32>()
+                    ))
+                    .fields(vec![
+                        (
+                            "Region",
+                            users
+                                .iter()
+                                .map(|u| u.region.as_ref().to_string())
+                                .collect::<Vec<String>>()
+                                .join("\n"),
+                            true,
+                        ),
+                        (
+                            "Online",
+                            users
+                                .iter()
+                                .map(|u| format!("{}", u.online_player_count))
+                                .collect::<Vec<String>>()
+                                .join("\n"),
+                            true,
+                        ),
+                    ])
+                    .footer(|f| f.text("Including players in private matches"))
+            })
+        })
+        .await?;
+    }
     Ok(())
 }
